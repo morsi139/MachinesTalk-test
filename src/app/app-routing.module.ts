1f6d3782c7ef77661760fe598f7e180ec04ef22d
morsi139 <morsi.derbel@esprit.tn>
2023-01-27 14:17:33 +0100

    api get

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LaunchListComponent } from './components/launch-list/launch-list.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LaunchListComponent } from './components/launch-list/launch-list.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RelativeTimePipe } from './relative-time/relative-time.pipe';
 
 
 
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     LaunchListComponent,
-    SearchComponent
+    SearchComponent, 
+    RelativeTimePipe
 
   ],
   imports: [
